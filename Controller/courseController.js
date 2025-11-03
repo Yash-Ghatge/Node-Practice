47da8695c8831eded303ba0d367083b02cb7290d
Yash-Ghatge <yashghatge012@gmail.com>
2025-11-03 20:05:53 +0530

    Update

--- Controller/courseController.js
+++ Controller/courseController.js
@@ -18,3 +18,19 @@ export const course = async ( req , res ) => {
         res.status(500).json({success : false , message : 'Internal Server'})
     }
 }
+
+export const getcourse = async ( req , res ) => {
+    try {
+        const page = parseInt(req.query.page) || 1
+        const limit = 5
+        const skip = (page - 1) * limit
+
+        const courses = await Course.find().skip(skip).limit(limit)
+        if (!courses) {
+            return res.status(400).json({success:true,data:courses})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:'Internal Server Error',success:false})
+    }
+}
